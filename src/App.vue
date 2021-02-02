afd20213268901272da3c6f004ed8c480bbd0abb
dawos78 <minimojo78@hotmail.com>
2021-02-03 03:56:28 +0700

    new

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,20 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-
+    <navbar></navbar>
     <router-view />
-
-    
   </div>
 </template>
 
+<script>
+import Vue from "vue";
+import navbar from "../src/components/navbar";
+export default Vue.extend({
+  components: {
+    navbar
+  }
+});
+</script>
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
