ace83406179641f644d850146986c4d91febf38b
shellchellly <shellshellgame@gmail.com>
2021-02-22 17:27:36 +0700

    up

--- src/views/PageSearch.vue
+++ src/views/PageSearch.vue
@@ -195,7 +195,7 @@ export default Vue.extend({
         {
           id: "6",
           url: "images/YP_LOGO2018-Y3.png",
-          seturl: "images/unnamed.png",
+          // seturl: "images/unnamed.png",
           company: "Yellow PAGES",
           jobcompany: "เปิดรับสมัครหลายอัตรา",
           time: "1.มก.64",
